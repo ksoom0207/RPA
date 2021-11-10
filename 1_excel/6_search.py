0378af185ebe903da1c00f4542fe52f9aa1e5bb1
숨 <ksoom0207@gmail.com>
2021-11-10 22:05:32 +0900

    1110

--- 1_excel/6_search.py
+++ 1_excel/6_search.py
@@ -6,3 +6,10 @@ for row in ws.iter_rows(min_row=2):
     # 영어점수 갖고오기
     if int(row[1].value) > 80:
         print(row[0].value, "번 학생은 영어천재")
+
+for row in ws.iter_rows(max_row=1):
+    for cell in row:
+        if cell.value == "영어":  # 영어를
+            cell.value = "컴퓨터"  # 컴퓨터로 바꿈
+
+wb.save("sample_modified.xlsx")
